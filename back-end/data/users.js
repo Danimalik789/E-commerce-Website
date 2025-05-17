b63addb9e7661e19913ac430d9e8eddd8d18fdd6
Danyal malik <danyalmalik789@gmail.com>
2025-05-17 23:42:06 +0500

    added three dummy users in data users.js

---
+++ back-end/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs"
+
+const users = [
+    {
+        name: 'Admin user',
+        email:' Admin@gmail.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'John Doe',
+        email:' John@gmail.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Jane Doe',
+        email:' jane@gmail.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+]
+
+export default users
