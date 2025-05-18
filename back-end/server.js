4b911be2c47a82615b0ccd6086f2b91421988323
Danyal malik <danyalmalik789@gmail.com>
2025-05-18 20:21:07 +0500

    added custom error handler and middlware

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
-import router from "./routes/productRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import connectDB from "./config/db.js"
 
 import productRoutes from "./routes/productRoutes.js"
@@ -10,6 +10,7 @@ dotenv.config()
 
 connectDB()
 
+// Initialize App
 const app = express()
 
 app.get("/", (req, res) => {
@@ -17,6 +18,9 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/products", productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
