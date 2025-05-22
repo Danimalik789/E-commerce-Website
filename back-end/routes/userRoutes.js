e5806241d3fbd12d2474a77f64293b771aef71de
Danyal malik <danyalmalik789@gmail.com>
2025-05-22 16:23:47 +0500

    created the user login functionality at front-end now user can sign in at that userInfo coming from the local storage

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,8 +1,15 @@
 import express from "express"
 const router = express.Router()
-import { authUser, getUserProfile } from "../controllers/userController.js"
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js"
 import { protect } from "../middleware/authMiddleware.js"
 
+router.route("/").post(registerUser)
+router.route("/profile").put(protect, updateUserProfile)
 router.post("/login", authUser)
 router.route("/profile").get(protect, getUserProfile)
 
---
+++ front-end/src/screens/LoginScreen.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react"
+import { Form, Button, Row, Col } from "react-bootstrap"
+import { Link, useNavigate, useLocation } from "react-router"
+import { useDispatch, useSelector } from "react-redux"
+import Loader from "../components/Loader"
+import Message from "../components/Message"
+import { login } from "../actions/userActions"
+import FormContainer from "../components/FormContainer"
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const redirect = new URLSearchParams(location.search).get("redirect") || "/"
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { loading, error, userInfo } = userLogin
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect)
+    }
+  }, [navigate, userInfo, redirect])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(login(email, password))
+  }
+
+  return (
+    <FormContainer>
+      <h1>Sign in</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter Your Email Address'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter Your password '
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type='submit' variant='primary'>
+          Sign in
+        </Button>
+      </Form>
+      <Row className='py-3'>
+        <Col>
+          New Customer?{" "}
+          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
+}
+
+export default LoginScreen
