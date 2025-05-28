a531c0f4d57a5ed777c7119e60092c5c6ba5e183
Danyal malik <danyalmalik789@gmail.com>
2025-05-28 14:36:00 +0500

    created the backend routes for getting user by id and updating it for admin only

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -122,7 +122,44 @@ const deleteUser = asyncHandler(async (req, res) => {
       message: "User removed",
     })
   } else {
-    rs.status(404)
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserByID = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password")
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+
+// @desc    Update user
+// @route   POST /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
     throw new Error("User not found")
   }
 })
@@ -134,4 +171,6 @@ export {
   getUserProfile,
   getUsers,
   deleteUser,
+  getUserByID,
+  updateUser,
 }
--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -42,6 +42,3 @@ userSchema.pre("save", async function (next) {
 
 const User = mongoose.model("User", userSchema)
 export default User
-
-
-
